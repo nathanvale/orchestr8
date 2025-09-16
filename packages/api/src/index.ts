856d02021984b9649f3842c078009a843044acf6
Nathan Vale <hi@nathanvale.com>
2025-09-16 12:23:35 +1000

    refactor: remove deprecated API package and related tests

--- packages/api/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @template/api - Remote cache and authentication utilities
- */
-
-export * from './remote-cache.js'
-export * from './utils/jwt.js'
--- packages/api/tsup.config.ts
+++
@@ -1,5 +0,0 @@
-import { createTsupConfig } from '../../tooling/build/tsup.base.js'
-
-export default createTsupConfig({
-  entry: ['src/index.ts', 'src/remote-cache.ts', 'src/utils/jwt.ts'],
-})
